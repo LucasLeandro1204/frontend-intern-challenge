62cf7df06928e0f734ee5568bd6df268db426ee8
Lucas Leandro <lucasleandro1204@gmail.com>
2017-07-30 21:28:30 -0300

    start

---
+++ webpack.mix.js
@@ -0,0 +1,24 @@
+const { mix } = require('laravel-mix');
+
+const resources = {
+  views: 'resources/views/',
+  js: 'resources/assets/js/',
+  scss: 'resources/assets/sass/',
+  images: 'resources/assets/images/',
+}
+
+const public = {
+  js: 'public/build/js/',
+  scss: 'public/build/css/',
+  images: 'public/build/images/',
+}
+
+mix.disableNotifications();
+
+mix.js(resources.js + 'app.js', public.js);
+mix.sass(resources.scss + 'app.scss', public.scss);
+
+mix.copy(resources.images, public.images)
+  .copy(resources.views + 'index.html', 'public/');
+
+mix.browserSync('frontend-intern-challenge.dev');
